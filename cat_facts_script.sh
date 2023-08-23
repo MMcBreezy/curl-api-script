d2842269f42ce3459c289e9e4d86c1c89e3a8610
mamacbride <michaelmcbride85@gmail.com>
2023-08-22 20:58:40 -0400

    first commit

---
+++ cat_facts_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+API_URL="https://catfact.ninja/fact"
+
+OUTPUT_FILE="Random_Cat_Facts.txt"
+
+echo -e "\n--- $(date) ---\n" >> "$OUTPUT_FILE"
+
+curl -s "$API_URL" >> "$OUTPUT_FILE"
+
+echo ""API response saved to $OUTPUT_FILE""
+
+#Running "./cat_facts_scripts.sh" will call the catfact API and store the response in the Random_Cat_Facts.txt file.
